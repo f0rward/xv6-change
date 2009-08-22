c0f039a537ccef50a69eaf86c053cb1fd1b3305c
jinyq06 <jinyq06@f63ad55a-8df9-11de-b999-7dfecaa767bb>
2009-08-22 13:08:48 +0000

    removed the influence of NCPU defined in params.h

--- trunk/param.h
+++ trunk/param.h
@@ -3,7 +3,7 @@
 #define NPROC        64  // maximum number of processes
 #define PAGE       4096  // granularity of user-space memory allocation
 #define KSTACKSIZE 8*PAGE  // size of per-process kernel stack
-#define NCPU          1  // maximum number of CPUs
+#define NCPU          1  // maximum number of CPUs, this parameter is not used.
 #define NOFILE       16  // open files per process
 #define NFILE       100  // open files per system
 #define NBUF         10  // size of disk block cache
