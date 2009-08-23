52b408aa6527363715e016666b48bc37edf78b78
jinyq06 <jinyq06@f63ad55a-8df9-11de-b999-7dfecaa767bb>
2009-08-23 15:48:00 +0000

    implemented firstfit, and preliminarily verified with simple demo in function test_firstfit(), to be further verified

--- firstfit.c
+++ firstfit.c
@@ -3,6 +3,7 @@
 
 free_area_t free_area;
 struct Page *mem_map;
+void test_firstfit();
 
 void init_memmap_FF(struct Page * base, unsigned long nr){
   struct Page* page=base;
@@ -13,6 +14,7 @@ void init_memmap_FF(struct Page * base, unsigned long nr){
   LIST_INSERT_HEAD(&free_area.free_list, page, lru);
   free_area.nr_free++;
   page->property = nr;
+  //test_firstfit();
 }
 
 struct Page * __alloc_pages_FF(int nr){
@@ -29,11 +31,12 @@ struct Page * __alloc_pages_FF(int nr){
   }
   if(count<=free_area.nr_free){
     LIST_REMOVE(page, lru);
-    free_area.nr_free--;
     struct Page* remain=page+nr;
     remain->property=page->property-nr;
     if(remain->property)
       LIST_INSERT_HEAD(&free_area.free_list, remain, lru);
+    else
+      free_area.nr_free--;
     return page;
   }else{
     return NULL;
@@ -46,7 +49,14 @@ void __free_pages_FF(struct Page * page, int nr){
     head->property=0;
     LIST_REMOVE(head, lru);
     LIST_INSERT_HEAD(&free_area.free_list, page, lru);
-  }else if(head==page-nr){
+  }else if(head==page-head->property){
+    struct Page* head_next = LIST_NEXT(head, lru);
+    if(page+nr==head_next){
+      head->property+=head_next->property;
+      head_next->property=0;
+      LIST_REMOVE(head_next, lru);
+      free_area.nr_free--;
+    }
     head->property+=nr;
     page->property=0;
   }else{
@@ -54,4 +64,28 @@ void __free_pages_FF(struct Page * page, int nr){
     page->property=nr;
     free_area.nr_free++;
   }
+}
+
+void print_cont_mem(){
+  int count=0;
+  struct Page* page=mem_map;
+  while(count<free_area.nr_free){
+    cprintf("free contiguous mem %x, size: %d pages\n", page_addr(page), page->property);
+    count++;
+    page=LIST_NEXT(page, lru);
+  }
+}
+
+void test_firstfit(){
+  print_cont_mem();
+  struct Page* p1=__alloc_pages_FF(1);
+  struct Page* head=LIST_FIRST(&free_area.free_list);
+  cprintf("after allocating a page, starting address of the contiguous mem becomes %x\n", page_addr(head));
+  struct Page* p2=__alloc_pages_FF(1);
+  __free_pages_FF(p1, 1);
+  cprintf("after freeing p1\n");
+  print_cont_mem();
+  __free_pages_FF(p2, 1);
+  cprintf("after freeing p2\n");
+  print_cont_mem();
 }
