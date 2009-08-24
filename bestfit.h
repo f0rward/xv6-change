eb651292e8c0db9502f363b68683c40d238f33d4
jinyq06 <jinyq06@f63ad55a-8df9-11de-b999-7dfecaa767bb>
2009-08-24 01:46:18 +0000

    implemented and tested bestfit algorithm, awaiting further tests, adding bestfit.c and bestfit.h

---
+++ bestfit.h
@@ -0,0 +1,19 @@
+#ifndef _BESTFIT_H
+#define _BESTFIT_H
+
+#include "pmap.h"
+
+typedef struct free_area{
+  page_list_head_t free_list;
+  unsigned long nr_free;
+} free_area_t;
+
+void init_memmap_BF(struct Page * base, unsigned long nr_free);
+
+extern struct Page * mem_map;
+
+struct Page* __alloc_pages_BF(int nr);
+
+void __free_pages_BF(struct Page* page, int nr);
+
+#endif
